06901db11959f48b303108120a202864c92ed905
Fairyghost007 <biranenini6762@gmail.com>
2025-01-05 15:43:37 +0000

    style:🎗upgraded design

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/key', (req, res) => {
+    res.json({ apiKey: process.env.WEATHER_API_KEY });
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
