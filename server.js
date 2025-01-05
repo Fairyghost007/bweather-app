494e4d4941388b8e194f32e7d64ac52af564d6d9
Fairyghost007 <biranenini6762@gmail.com>
2025-01-05 16:07:26 +0000

    🔗fix(link):server link problem-2

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+const cors = require('cors');
+
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/key', (req, res) => {
@@ -13,5 +16,6 @@ app.get('/api/key', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
